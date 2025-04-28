03f3ebf033f8101a12a46d765291a1bff187b557
tichane <ichane.theo@gmail.com>
2025-04-28 17:48:38 +0200

    check online users

--- handler/websocketHandler.go
+++ handler/websocketHandler.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"real-time-forum/database"
@@ -17,10 +16,10 @@ var upgrader = websocket.Upgrader{
 }
 
 type Message struct {
-	Type string `json:"type"`
-	Sender string `json:"sender"`
+	Type     string `json:"type"`
+	Sender   string `json:"sender"`
 	Receiver string `json:"receiver"`
-	Content string `json:"content"`
+	Content  string `json:"content"`
 }
 
 func WebSocketHandler(w http.ResponseWriter, r *http.Request, hub *utils.Hub) {
@@ -32,7 +31,6 @@ func WebSocketHandler(w http.ResponseWriter, r *http.Request, hub *utils.Hub) {
 	}
 	defer conn.Close()
 
-	
 	cookie, err := r.Cookie("session")
 
 	if err != nil {
@@ -42,46 +40,27 @@ func WebSocketHandler(w http.ResponseWriter, r *http.Request, hub *utils.Hub) {
 	session := database.GetUserIdBySession(cookie.Value)
 	nickname := database.GetNicknameByUserId(session)
 
-	var message Message
-	err = (conn.ReadJSON(&message))
-	if err != nil {
-		fmt.Println("Error reading JSON:", err)
-	} else {
-		if message.Type == "logout" || message.Type == "login" {
-			hub.BroadcastMessage([]byte(message.Content))
-		} else {
-			hub.SendMessage([]byte(message.Content), message.Receiver,message.Sender)
-		}
-	}
-
 	hub.RegisterClient(conn, nickname)
 
 	// Broadcast the message to all clients
 	hub.BroadcastMessage([]byte(fmt.Sprintf("%s has joined the chat", nickname)))
 	fmt.Println("Client connected:", nickname)
-	
 
 	// Listen for messages from the client
-	for {	
-		_, _, err := conn.ReadMessage()
+	for {
+		var message Message
+		err = (conn.ReadJSON(&message))
 		if err != nil {
-			fmt.Println("", err)
-			break
+			fmt.Println("Error reading JSON:", err)
+			return
+		} else {
+			if message.Type == "logout" || message.Type == "login" {
+				hub.BroadcastMessage([]byte(message.Content))
+			} else {
+				hub.SendMessage([]byte(message.Content), message.Receiver, message.Sender)
+			}
 		}
-
 
 	}
 
-	// À la fin de la fonction WebSocketHandler
-hub.UnregisterClient(conn)
-fmt.Println(nickname, "s'est déconnecté — broadcast en cours")
-// Créer un message JSON spécifique pour la déconnexion
-disconnectMsg := map[string]string{
-    "connexion": fmt.Sprintf("%s has left the chat", nickname),
-    "type": "disconnect",
-    "user": nickname,
-}
-jsonMsg, _ := json.Marshal(disconnectMsg)
-hub.BroadcastMessage(jsonMsg)
-	// Close the connection
 }
